bc39eaffd41b08b23499d8d1bd164abbc0e9abef
gcool-info <george.koulouris1@gmail.com>
2014-11-27 20:47:41 +0000

    Added animations for webkit + 2nd page of participate

---
+++ server/new-email-subscriber.php
@@ -0,0 +1,14 @@
+<?php
+	require("lib/mailchimp/MailChimp.php");
+	
+	$MailChimp = new \Drewm\MailChimp('e62c09670ddd090432b614828427f65b-us9');
+	$result = $MailChimp->call('lists/subscribe', array(
+				'id'                => 'e2e7d840aa',
+                'email'             => array('email'=> $_POST["email"]),
+                'double_optin'      => false,
+                'update_existing'   => true,
+                'replace_interests' => false,
+                'send_welcome'      => true,
+            ));
+	echo json_encode($result);
+?>
