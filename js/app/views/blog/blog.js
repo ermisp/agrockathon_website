e44fd03bbf8bd773a2a04b2195cc1d2d5ca9b63b
gcool-info <george.koulouris1@gmail.com>
2014-12-07 17:28:48 +0000

    Youtube videos can be embedded in blog

--- js/app/views/blog/blog.js
+++ js/app/views/blog/blog.js
@@ -22,7 +22,7 @@ define(function (require) {
 
             this.$el.append(template());
 
-            for (var i = this.collection.length - 1; i >= 0 ; i--) {
+            for (var i = 0; i < this.collection.length; i++) {
                 inspirations[i] = new SingleArticleView({el: $('.inspirations'), model: this.collection.models[i]});
                 inspirations[i].render();
             }
@@ -46,12 +46,22 @@ define(function (require) {
             var inspirationID = $(e.target).parent().closest('.single-inspiration').attr('id');
 
             /* Define a regular expression to find links within the text & replace them with hyperlinks */
-            //var urlCheck = /\b(([\w-]+:\/\/?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|\/)))/i;
+            var urlCheck = /\b(([\w-]+:\/\/?|www[.])[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|\/)))/m;
+
+            /* Define a video regex to show the thumbnail if video (only youtube for now) is in media */
+            var videoRegEx = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i; 
+
+            var media = this.collection.get(inspirationID).get('media');
+
+            if (media.match(videoRegEx)) {
+                 media = '<iframe class="tiny-margin-top" width="560" height="315" src="http://www.youtube.com/embed/' + media.match(videoRegEx)[1] + '?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>';
+            } else {
+                media = '<img class="tiny-margin-top" src="' + media +'" />';
+            }
 
             /* Fill the modal with the information from this inspiration */
             var title = '<h4><strong>' + this.collection.get(inspirationID).get('title') + '</strong></h4>';
-            var media = '<img class="tiny-margin-top" src="' + this.collection.get(inspirationID).get('media') +'" />';
-            var body = '<div class="width-80 centered tiny-margin-top">' + this.collection.get(inspirationID).get('body') +'</div>';
+            var body = '<div class="width-80 centered tiny-margin-top">' + this.collection.get(inspirationID).get('body').replace(urlCheck, '<a class="green" href="$&" target="_blank">$&</a>') +'</div>';
 
             $('#modal-content').html(title + media + body);
             $('#inspirationModal').foundation('reveal', 'open');
